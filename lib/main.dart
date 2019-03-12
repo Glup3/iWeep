4c14661b2c462af0045606b588cb7205f275998b
Phuc Tran <glup3.tran@gmail.com>
2019-03-12 08:40:44 +0100

    resolved conflict

--- lib/main.dart
+++ lib/main.dart
@@ -157,14 +157,6 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   void onTabTapped(int index) {
-    bool value;
-
-    if (index == 0) {
-      value = false;
-    } else {
-      value = true;
-    }
-
     setState(() {
       _currentIndex = index;
       _isHidden = index == 0 ? false : true;
